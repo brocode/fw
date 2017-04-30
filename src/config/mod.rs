a7aaf51fc6eb290b5a3360e82bb536ff1c187c17
mriehl <max@flatmap.ninja>
2017-04-30 08:43:00 +0200

    [feat] inform user about new priority feature

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -80,6 +80,20 @@ impl Config {
     self.resolve_from_tags(|t| t.clone().after_clone, maybe_tags, logger)
   }
 
+  fn tag_priority_or_fallback(&self, name: &str, tag: &Tag, logger: &Logger) -> u8 {
+    match tag.priority {
+    None => {
+      warn!(logger, r#"No tag priority set, will use default (50).
+Tags with low priority are applied first and if they all have the same priority
+they will be applied in alphabetical name order so it is recommended you make a
+conscious choice and set the value."#;
+            "tag_name" => format!("{}", name), "tag_def" => format!("{:?}", tag));
+      50
+    }
+    Some(p) => p,
+    }
+  }
+
   fn resolve_from_tags<F>(&self, resolver: F, maybe_tags: Option<BTreeSet<String>>, logger: &Logger) -> Option<String>
     where F: Fn(&Tag) -> Option<String>
   {
@@ -100,7 +114,7 @@ impl Config {
                       Some(actual_tag) => {
                         resolver(actual_tag)
                           .clone()
-                          .map(|val| (val, actual_tag.priority.unwrap_or(50)))
+                          .map(|val| (val, self.tag_priority_or_fallback(t, actual_tag, &logger)))
                       }
                       })
             .collect();
