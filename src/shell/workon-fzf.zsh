3ab1551c9577e30b69118c9422b47064f9ef184f
Patrick Haun <bomgar85@googlemail.com>
2017-05-15 14:08:09 +0200

    [feat] Preview script

--- src/shell/workon-fzf.zsh
+++ src/shell/workon-fzf.zsh
@@ -1,5 +1,5 @@
 workon () {
-  PROJECT="$(fw -q ls | fzf --cycle --query=$1 --color=light)"
+  PROJECT="$(fw -q ls | fzf --cycle --query=$1 --color=light --preview-window=top:50% --preview='~/.cargo/bin/fw -q gen-workon {}') --no-mouse"
   SCRIPT="$(~/.cargo/bin/fw -q gen-workon $PROJECT)";
   if [ $? -eq 0 ]; then
     eval "$SCRIPT";
