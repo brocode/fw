432247308ca157ea18d45914cbb4bedbb1a7773d
Patrick Haun <bomgar85@googlemail.com>
2017-09-11 18:00:26 +0200

    [build] Add musl package build

---
+++ build_packages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e -u -o pipefail
+
+name=fw
+
+rm -rf target
+docker build rust-musl-builder -t fw-rust-musl-builder
+docker run --rm -it -v "$(pwd)":/home/rust/src fw-rust-musl-builder cargo build --release
+
+target_dir="target/x86_64-unknown-linux-musl/release"
+
+version=$($target_dir/$name --version | cut -d ' ' -f 2)
+
+docker run -v $(readlink -e ./$target_dir):/release -it --rm  alanfranz/fpm-within-docker:centos-7      fpm -s dir -t rpm -n $name -p /release/$name.rpm -v $version /release/$name=/usr/bin/$name
+docker run -v $(readlink -e ./$target_dir):/release -it --rm  alanfranz/fpm-within-docker:ubuntu-zesty  fpm -s dir -t deb -n $name -p /release/$name.deb -v $version /release/$name=/usr/bin/$name
+
+
+
