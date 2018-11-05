70b059410d122203a8eb2b619c552994b4d16e2a
Marko Jahn <marko.jahn@21re.de>
2018-11-05 14:17:15 +0100

    [chore] cleanup delete tag, error message to add_tag when tagname unknown

--- src/tag/mod.rs
+++ src/tag/mod.rs
@@ -42,7 +42,6 @@ pub fn create_tag(
 
 pub fn delete_tag(maybe_config: Result<Config>, tag_name: &str, logger: &Logger) -> Result<()> {
   let mut config: Config = maybe_config?;
-  let mut config2 : Config = config.clone();
   let mut tags: BTreeMap<String, Tag> = config.settings.tags.unwrap_or_else(BTreeMap::new);
 
   // remove tags from projects
@@ -61,8 +60,8 @@ pub fn delete_tag(maybe_config: Result<Config>, tag_name: &str, logger: &Logger)
 
   info!(logger, "Delete tag"; "tag" => tag_name);
   if tags.remove(tag_name).is_some() {
-    config2.settings.tags = Some(tags);
-    config::write_config(config2, logger)
+    config.settings.tags = Some(tags);
+    config::write_config(config, logger)
   } else {
     Ok(())
   }
@@ -82,15 +81,24 @@ pub fn add_tag(maybe_config: Result<Config>, project_name: String, tag_name: Str
   let mut config: Config = maybe_config?;
   if let Some(mut project) = config.projects.get(&project_name).cloned() {
     info!(logger, "Add tag to project"; "tag" => &tag_name, "project" => &project_name);
+<<<<<<< HEAD
     let mut tags: BTreeMap<String, Tag> = config.settings.tags.clone().unwrap_or_else(BTreeMap::new);
     if tags.contains_key(&tag_name) {
         let mut new_tags: BTreeSet<String> = project.tags.clone().unwrap_or_else(BTreeSet::new);
+=======
+    let mut new_tags: BTreeSet<String> = project.tags.clone().unwrap_or_else(BTreeSet::new);
+    if new_tags.contains(&tag_name) {
+>>>>>>> [chore] cleanup delete tag, error message to add_tag when tagname unknown
         new_tags.insert(tag_name);
         project.tags = Some(new_tags);
         config.projects.insert(project_name, project);
         config::write_config(config, logger)
     } else {
+<<<<<<< HEAD
         Err(ErrorKind::UserError(format!("Unknown tag {}", tag_name)).into())
+=======
+            Err(ErrorKind::UserError(format!("Unknown tag {}", tag_name)).into())
+>>>>>>> [chore] cleanup delete tag, error message to add_tag when tagname unknown
     }
 
   } else {
