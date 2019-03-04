e6a171a9c429c17535faa515e33340d8677a6143
Patrick Haun <patrick@bomgar.email>
2019-03-04 12:35:50 +0100

    [build] update ubuntu packaging

--- package.sh
+++ package.sh
@@ -16,7 +16,7 @@ target_dir="target/x86_64-unknown-linux-musl/release"
 version=$($target_dir/$name --version | cut -d ' ' -f 2)
 
 docker run -v $(readlink -e ./$target_dir):/release -it --rm  alanfranz/fpm-within-docker:centos-7      fpm -s dir -t rpm -n $name -p /release/$name.rpm -v $version /release/$name=/usr/bin/$name
-docker run -v $(readlink -e ./$target_dir):/release -it --rm  alanfranz/fpm-within-docker:ubuntu-zesty  fpm -s dir -t deb -n $name -p /release/$name.deb -v $version /release/$name=/usr/bin/$name
+docker run -v $(readlink -e ./$target_dir):/release -it --rm  alanfranz/fpm-within-docker:ubuntu-bionic  fpm -s dir -t deb -n $name -p /release/$name.deb -v $version /release/$name=/usr/bin/$name
 
 
 
