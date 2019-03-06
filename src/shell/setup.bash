eeded180e4c16b36a467368e7b5d65eb13e2af26
James Simpson <james.simpson9339@gmail.com>
2019-03-06 17:30:12 -0500

    rewite completion in setup script
    Completion script is based on git's completion
    script. Everything is wrapped in __fw_complete

--- src/shell/setup.bash
+++ src/shell/setup.bash
@@ -1,133 +1,269 @@
 command -v fw >/dev/null 2>&1 &&
 
-_fw()
+__fw_complete()
 {
-    local cur prev pprev
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
-    pprev="${COMP_WORDS[COMP_CWORD-2]}"
+    __fw_comp()
+    {
+        local cur_="${3-$cur}"
 
-    # looking for the command
-    local nwords=${#COMP_WORDS[@]}
-    local cmd_i cmd dd_i
-    for (( cmd_i=1; cmd_i<$nwords; cmd_i++ )); do
-		if [[ ! "${COMP_WORDS[$cmd_i]}" =~ ^[+-] ]]; then
-			cmd="${COMP_WORDS[$cmd_i]}"
-			break
-		fi
-	done
-
-    # Find the location of the -- separator.
-	for (( dd_i=1; dd_i<$nwords-1; dd_i++ )); do
-		if [[ "${words[$dd_i]}" = "--" ]]; then
-			break
-		fi
-	done
-
-    local opt_help='-h --help'
-    local opt_verbose='-v --verbose'
-    local opt_quiet='-q'
-    local opt_common="$opt_help $opt_verbose $opt_quiet"
-    local opt_proj_cfg="--after-clone --after-workon --override-path"
-    local opt_parallel="-p"
-    local opt_tag="-t --tag"
-    local opt_json="-j --json"
-    local opt_purge="-p --purge-directory"
-
-    local opt__nocmd="$opt_common -V --version"
-    local opt__add="$opt_common $opt_proj_cfg"
-    local opt__add_remote="$opt_common"
-    local opt__export_by_tag="$opt_common"
-    local opt__export_project="$opt_common"
-    local opt__export_tag="$opt_common"
-    local opt__foreach="$opt_common $opt_parallel $opt_tags"
-    local opt__gitlab_import="$opt_common"
-    local opt__help="$opt_common"
-    local opt__import="$opt_common"
-    local opt__inspect="$opt_common $opt_json"
-    local opt__ls="$opt_common"
-    local opt__org_import="$opt_common -a --include-archived"
-    local opt__print_path="$opt_common"
-    local opt__projectile="$opt_common"
-    local opt__remove="$opt_common $opt_purge"
-    local opt__remove_remote="$opt_common"
-    local opt__setup="$opt_common"
-    local opt__sync="$opt_common $opt_parallel --no-ff-merge -q --no-progress-bar -n --only-new"
-    local opt__tag="$opt_common"
-    local opt__update="$opt_common $opt_proj_cfg --git-url"
-
-    if [[ $COMP_CWORD -gt $dd_i ]]; then
-        # completion after -- separator
-        COMPREPLY=( $( compgen -f  -- "${COMP_WORDS[${COMP_CWORD}]}" ) )
-    elif [[ $COMP_CWORD -le $cmd_i ]]; then
-        # at either before or at the command
-        if [[ "$cur" == -* ]]; then
-            COMPREPLY=( $( compgen -W "${opt___nocmd}" -- "$cur" ) )
-        else
-            COMPREPLY=( $( compgen -W "$(__fw_commands)" -- "$cur" ) )
-        fi
-    else
-        case "${prev}" in
-            help) COMPREPLY=( $( compgen -W "$(__fw_commands)" -- "$cur" ) ) ;;
-            tag) COMPREPLY=( $( compgen -W "$(__fw_tag_commands)" -- "$cur" ) ) ;;
-            remove) COMPREPLY=( $( compgen -W "$(__fw_projects)" -- "$cur" ) ) ;;
+        case "$cur_" in
+            --*=)
+                ;;
             *)
-                local opt_var=opt__${cmd//-/_}
-				if [[ -z "${!opt_var}" ]]; then
-                    # fallback to filename completion
-                    COMPREPLY=( $( compgen -f  -- "${COMP_WORDS[${COMP_CWORD}]}" ) )
-                else
-                    COMPREPLY=( $( compgen -W "${!opt_var}" -- "$cur" ) )
-                fi
+                local c i=0 IFS=$' \t\n'
+                for c in $1; do
+                    c="$c${4-}"
+                    if [[ $c == "$cur_"* ]]; then
+                        case $c in
+                            --*=*|*.) ;;
+                            *) c="$c " ;;
+                        esac
+                        COMPREPLY[i++]="${2-}$c"
+                    fi
+                done
                 ;;
         esac
-    fi
+    }
 
-    return 0
+    __fw_commands()
+    {
+        local cmds=(
+            'add-remote'
+            'add'
+            'export-by-tag'
+            'export-project'
+            'export-tag'
+            'foreach'
+            'gitlab-import'
+            'help '
+            'import'
+            'inspect'
+            'ls'
+            'org-import'
+            'print-path'
+            'projectile'
+            'remove-remote'
+            'remove'
+            'reworkon'
+            'setup'
+            'sync'
+            'tag'
+            'update'
+        )
+        echo "${cmds[@]}"
+    }
+
+    __fw_tags()
+    {
+        local tags=()
+        while read line; do
+            tags+=($line)
+        done < <(fw -q tag ls)
+        echo ${tags[@]}
+    }
+    
+    __find_on_cmdline() {
+        local word subcommand c=1
+        while [ $c -lt $cword ]; do
+            word="${words[c]}"
+            for subcommand in $1; do
+                if [ "$subcommand" = "$word" ]; then
+                    echo "$subcommand"
+                    return
+                fi
+            done
+            ((c++))
+        done
+    }
+
+    _fw_add() {
+        case "$cur" in
+            --*) __fw_comp "--after-clone --after-workon --override-path" ; return ;;
+        esac
+    }
+
+    _fw_add_remote() {
+        __fw_comp "$(__fw_projects)"
+    }
+
+    _fw_export_by_tag () {
+        __fw_comp "$(__fw_tags)"
+    }
+
+    _fw_export_project () {
+        __fw_comp "$(__fw_projects)"
+    }
+
+    _fw_export_tag () {
+        __fw_comp "$(__fw_tags)"
+    }
+
+    _fw_foreach () {
+        case "$prev" in 
+            --tag|-t) __fw_comp "$(__fw_tags)" ; return ;;
+        esac
+
+        case "$cur" in
+            --*) __fw_comp "--parallel --tag" ; return ;;
+        esac
+    }
+
+    _fw_help () {
+        __fw_comp "$(__fw_commands)"
+    }
+
+    _fw_import () {
+        __fw_comp "$(__fw_projects)"
+    }
+
+    _fw_inspect () {
+        case "$cur" in
+            --*) __fw_comp "--json" ; return ;;
+        esac
+
+        __fw_comp "$(__fw_projects)"
+    }
+
+    _fw_org_import () {
+        case "$cur" in
+            --*) __fw_comp "--include-archived" ; return ;;
+        esac
+    }
+
+    _fw_print_path () {
+        __fw_comp "$(__fw_projects)"
+    }
+
+    _fw_remove_remote () {
+        __fw_comp "$(__fw_projects)"
+    }
+
+    _fw_remove () {
+        case "$cur" in
+            --*) __fw_comp "--purge-directory" ; return ;;
+        esac
+
+        __fw_comp "$(__fw_projects)"
+    }
+
+    # _fw_reworkon() {
+    # }
+
+    _fw_sync () {
+        case "$cur" in
+            --*) __fw_comp "--no-ff-merge --no-progress-bar --only-new --parallelism" ; return ;;
+        esac
+    }
+
+    _fw_tag () {
+        local subcommands='add autotag help ls rm tag-project untag-project '
+        local subcommand="$(__find_on_cmdline "$subcommands")"
+        case "$subcommand,$cur" in
+            ,*) __fw_comp "$subcommands" ;;
+            *)
+                local func="_fw_tag_${subcommand//-/_}"
+                declare -f $func >/dev/null && $func && return
+            ;;
+        esac
+    }
+
+    _fw_tag_add() {
+        case "$cur" in
+            --*) __fw_comp "--after-clone --after-workon --override-path" ; return ;;
+        esac
+        __fw_comp "$(__fw_tags)"
+    }
+
+    _fw_tag_autotag() {
+        case "$cur" in
+            --*) __fw_comp "--parallel" ; return ;;
+        esac
+        __fw_comp "$(__fw_tags)"
+    }
+
+    _fw_tag_help() {
+        __fw_comp 'add autotag ls rm tag-project untag-project'
+    }
+
+    _fw_tag_ls() {
+        __fw_comp "$(__fw_projects)"
+    }
+
+    _fw_tag_rm() {
+        __fw_comp "$(__fw_tags)"
+    }
+
+    _fw_tag_tag_project() {
+        local project="$(__find_on_cmdline "$(__fw_projects)")"
+        if [ -z "$project" ]; then
+            __fw_comp "$(__fw_projects)"
+        else
+            __fw_comp "$(__fw_tags)"
+        fi
+    }
+
+    _fw_tag_untag_project() {
+        local project="$(__find_on_cmdline "$(__fw_projects)")"
+        if [ -z "$project" ]; then
+            __fw_comp "$(__fw_projects)"
+        else
+            __fw_comp "$(__fw_tags)"
+        fi
+    }
+
+    _fw_update () {
+        case "$prev" in
+            --*) return ;;
+        esac
+
+        case "$cur" in
+            --*) __fw_comp "--after-clone --after-workon --git-url --override-path" ; return ;;
+        esac
+
+        __fw_comp "$(__fw_projects)"
+    }
+
+    # ---------------------------------------------------------------------------------------------------
+
+    __fw_main()
+    {
+        local i command c=1
+        while [ $c -lt $cword ]; do
+            i="${words[c]}"
+            case $i in
+                --help) command="help"; break ;;
+                -*) ;;
+                *) command="$i"; break ;;
+                esac
+            ((c++))
+        done
+
+        if [ -z "$command" ]; then
+            case "$cur" in
+            --*)   __fw_comp " --help" ;;
+            *)     __fw_comp "$(__fw_commands)" ;;
+            esac
+            return
+        fi
+
+        local completion_func="_fw_${command//-/_}"
+        declare -f $completion_func >/dev/null && $completion_func && return
+    }
+
+    __fw_wrap()
+    {
+        local cur words cword prev
+        _get_comp_words_by_ref -n =: cur words cword prev
+        __fw_main
+    }
+
+    complete -o bashdefault -o default -o nospace -F __fw_wrap $1 2>/dev/null \
+        || complete -o default -o nospace -F __fw_wrap $1
 } &&
-complete -F _fw fw
 
-__fw_commands()
-{
-    local cmds=(
-        'sync'
-        'setup'
-        'import'
-        'add'
-        'add-remote'
-        'remove-remote'
-        'remove'
-        'foreach'
-        'projectile'
-        'ls'
-        'inspect'
-        'update'
-        'tag'
-        'export-project'
-        'export-by-tag'
-        'export-tag'
-        'print-path'
-        'org-import'
-        'gitlab-import'
-    )
-    echo "${cmds[@]}"
-}
+__fw_complete fw
 
-__fw_tag_commands()
-{
-    local cmds=(
-        'add'
-        'autotag'
-        'help'
-        'ls'
-        'rm'
-        'tag-project'
-        'untag-project'
-    )
-    echo "${cmds[@]}"
-}
-
+# This is needed for workon's completion
 __fw_projects()
 {
     local projects=()
