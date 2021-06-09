4aae1aabe4bbd5ba033000592de1294fba40b141
Nigel-Lee Volkmann <nigel-lee.volkmann@dpdhl.com>
2021-06-09 08:24:25 +0200

    [feat] Print warning for addition of more than 50 projects

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -234,7 +234,16 @@ For further information please have a look at our README https://github.com/broc
         .arg(Arg::with_name("PROJECT_NAME").value_name("PROJECT_NAME").index(1).required(true)),
     )
     .subcommand(SubCommand::with_name("projectile").about("Write projectile bookmarks"))
-    .subcommand(SubCommand::with_name("intellij").about("Add projects to intellijs list of recent projects"))
+    .subcommand(
+      SubCommand::with_name("intellij")
+        .about("Add projects to intellijs list of recent projects")
+        .arg(
+          Arg::with_name("no-warn")
+            .long("no-warn")
+            .short("-n")
+            .help("Disables warning message if more than 50 projects would be added"),
+        ),
+    )
     .subcommand(
       SubCommand::with_name("ls").about("List projects").arg(
         Arg::with_name("tag")
